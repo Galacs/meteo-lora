a47a91f57b226755e5a9d7d97f3849d02a032c15
Rémi Ait-Younes <remi@ait-younes.fr>
2026-04-27 08:14:04 +0200

    clean code formatting

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -9,8 +9,6 @@ float frequency = 868.1;
 void setup() {
 
   Serial.begin(115200);
-  while (!Serial)
-    ; // Wait for serial port to be available
   Serial.println("Start Sketch");
   if (!rf95.init())
     Serial.println("init failed");
@@ -34,18 +32,9 @@ void setup() {
 
 void loop() {
   if (rf95.available()) {
-    /*uint8_t data[] = "english or spanish ?";
-    rf95.send(data, sizeof(data));
-    rf95.waitPacketSent();
-    Serial.println("Sent a reply");*/
-    // Should be a message for us now
-    // uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     weather_data_t msg;
-    // memcpy(phrase, msg.buf, 6);
     uint8_t len = sizeof(msg);
     if (rf95.recv((uint8_t *)&msg, &len)) {
-      // RH_RF95::printBuffer("request: ", buf, len);
-      // Serial.printf("Values: %d, %d\n", msg.lastRSSI, msg.lastSNR);
       Serial.print("SI Temperature: ");
       Serial.println(msg.si7021_temperature);
       Serial.print("SI Humidité: ");
@@ -65,17 +54,6 @@ void loop() {
       Serial.print((" an RRSI of: "));
       Serial.println(rf95.lastRssi());
     }
-
-    // Send a reply
-    // uint8_t data[] = "english or spanish ?";
-    // rf95.send((uint8_t *)&msg, sizeof(msg));
-    // Serial.println(("sent"));
-    // rf95.waitPacketSent();
-    // Serial.println("Sent a reply");
-
-    // } else {
-    //   Serial.println("recv failed");
-    // }
   }
   delay(10);
 }
--- station/src/main.cpp
+++ station/src/main.cpp
@@ -3,8 +3,8 @@
 
 #include "messages.h"
 
-#include <SPI.h>
 #include <Adafruit_Si7021.h>
+#include <SPI.h>
 #include <SparkFunMPL3115A2.h>
 #include <SparkFun_Weather_Meter_Kit_Arduino_Library.h>
 
@@ -21,11 +21,8 @@ SFEWeatherMeterKit weatherMeterKit(windDirectionPin, windSpeedPin, rainfallPin);
 RH_RF95 rf95;
 float frequency = 868.1;
 void setup() {
-
   weatherMeterKit.begin();
   Serial.begin(115200);
-  while (!Serial)
-    ; // Wait for serial port to be available
   Serial.println("Start Sketch");
   if (!rf95.init())
     Serial.println("init failed");
@@ -46,42 +43,19 @@ void setup() {
   Serial.print("Listening on frequency: ");
   Serial.println(frequency);
 
-if (!sensor.begin()) {
+  if (!sensor.begin()) {
     Serial.println("Did not find Si7021 sensor!");
-    while (true)
-      ;
+    while (true);
   }
 
-
-
   myPressure.begin(); // Get sensor online
-
   myPressure.setModeBarometer(); // Measure pressure in Pascals from 20 to 110 kPa
-  
   myPressure.setOversampleRate(7); // Set Oversample to the recommended 128
-  myPressure.enableEventFlags(); // Enable all three pressure and temp event flags 
-  
-
-
-
-
+  myPressure.enableEventFlags(); // Enable all three pressure and temp event flags
 }
 
-// struct __attribute__((packed)) msg_t {
-//   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN - sizeof(int) - sizeof(int16_t)];
-//   int lastSNR;
-//   int16_t lastRSSI;
-// };
-
 void loop() {
-  // if (rf95.available()) {
   if (true) {
-    /*uint8_t data[] = "english or spanish ?";
-    rf95.send(data, sizeof(data));
-    rf95.waitPacketSent();
-    Serial.println("Sent a reply");*/
-    // Should be a message for us now
-    // uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     weather_data_t msg;
     msg.si7021_humidity = sensor.readHumidity();
     msg.si7021_temperature = sensor.readTemperature();
@@ -92,49 +66,23 @@ void loop() {
     msg.wind_speed = weatherMeterKit.getWindSpeed();
     msg.rain_fall = weatherMeterKit.getTotalRainfall();
 
-
-    // Only print once per second
-    delay(1000);
-
-    // uint8_t *phrase = "salut";
-    // memcpy(phrase, msg.buf, 6);
-    // strcpy(phrase, msg.buf);
-    // uint8_t len = sizeof(msg);
-    // if (rf95.recv((uint8_t *)&msg, &len)) {
-    // RH_RF95::printBuffer("request: ", buf, len);
-    // Serial.print("got request: ");
-    // String a = String((char *)buf.buf);
-    // Serial.println(a);
-    // Serial.print((" with an SNR of: "));
-    // Serial.print(rf95.lastSNR());
-    // Serial.print((" an RRSI of: "));
-    // Serial.println(rf95.lastRssi());
-
-    // Send a reply
-    // uint8_t data[] = "english or spanish ?";
     rf95.send((uint8_t *)&msg, sizeof(msg));
     rf95.waitPacketSent();
     Serial.println(("sent"));
     delay(1000);
-    // Serial.println("Sent a reply");
-
-    // } else {
-    //   Serial.println("recv failed");
-    // }
 
     if (++loopCnt == 30) {
-    enableHeater = !enableHeater;
-    sensor.heater(enableHeater);
-    Serial.print("Heater Enabled State: ");
-    if (sensor.isHeaterEnabled())
-      Serial.println("ENABLED");
-    else
-      Serial.println("DISABLED");
-       
-    loopCnt = 0;
+      enableHeater = !enableHeater;
+      sensor.heater(enableHeater);
+      Serial.print("Heater Enabled State: ");
+      if (sensor.isHeaterEnabled())
+        Serial.println("ENABLED");
+      else
+        Serial.println("DISABLED");
+
+      loopCnt = 0;
     }
-  Serial.println("Waiting...");
-  delay(100);
+    Serial.println("Waiting...");
+    delay(100);
   }
 }
-
