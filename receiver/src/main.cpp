58c808a19cbe4aa0387ad2b19f13007e8a5cec75
HorizonGa111 <102918522+HorizonGa111@users.noreply.github.com>
2026-04-29 16:13:56 +0200

    receiver: ajout de l'affichage led

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -2,12 +2,17 @@
 #include <RH_RF95.h>
 #include <SPI.h>
 #include "messages.h"
+#include <LiquidCrystal.h>
 
 // Singleton instance of the radio driver
 RH_RF95 rf95;
 float frequency = 868.1;
-void setup() {
+const int rs = 17, en = 18, d4 = 19, d5 = 14, d6 = 16, d7 = 15;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+void setup() {
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
   Serial.begin(115200);
   Serial.println("Start Sketch");
   if (!rf95.init())
@@ -28,9 +33,12 @@ void setup() {
 
   Serial.print("Listening on frequency: ");
   Serial.println(frequency);
+    // set up the LCD's number of columns and rows:
+
 }
 
 void loop() {
+
   if (rf95.available()) {
     weather_data_t msg;
     uint8_t len = sizeof(msg);
@@ -55,7 +63,18 @@ void loop() {
       Serial.print(rf95.lastSNR());
       Serial.print((" an RRSI of: "));
       Serial.println(rf95.lastRssi());
+      
+      lcd.setCursor(0, 0);
+      lcd.print("TP: ");
+      lcd.print(msg.mpl_temperature);
+      lcd.print(" C");
+      lcd.setCursor(0, 1);
+      lcd.print("PR: ");
+      lcd.print(msg.mpl_pressure);
+      lcd.print(" Pa");
+
     }
   }
   delay(10);
+
 }
