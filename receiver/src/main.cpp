1e0f837a93a38129cfd44cf3cf307917baa422d8
Rémi Ait-Younes <remi@ait-younes.fr>
2026-06-10 12:26:36 +0200

    receiver: import pin definitions

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -6,6 +6,20 @@
 #include <WiFi.h>
 #include "messages.h"
 
+#define LORA_RST_PIN 24
+#define LORA_DIO1_PIN 25
+#define LORA_BUSY_PIN 27
+#define LORA_DIO3_PIN 26
+#define LORA_CS_PIN 3
+#define LORA_SCK_PIN 2
+#define LORA_MISO_PIN 4
+#define LORA_MOSI 5
+
+#define SCL_PIN 6
+#define SDA_PIN 7
+
+#define RGB_PIN 23
+
 #define MQTT_PORT 1883
 
 // CA Let's encrypt pour MQTT TLS. cf: https://letsencrypt.org/certificates/
